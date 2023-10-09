a25979916713f31e3eb452b363337fe67ac265b4
Irman Zaf <irman_zafyree_bin_irmanizam@s2019.ssts.edu.sg>
2023-10-10 04:06:13 +0800

    almost done with main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +15,7 @@ var imageList = [
 ];
 
 var _pageController = PageController(initialPage: 0);
+var _pc = PanelController();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -99,32 +102,32 @@ class _HomeScreenState extends State<HomeScreen> {
                       icon: Icon(
                         Icons.account_box,
                         color: selectedIndex == 0
-                            ? color.onPrimary
-                            : color.inversePrimary,
+                            ? color.inversePrimary
+                            : Colors.white,
                       ),
                       label: "Account"),
                   NavigationDestination(
                       icon: Icon(
                         Icons.person,
                         color: selectedIndex == 1
-                            ? color.onPrimary
-                            : color.inversePrimary,
+                            ? color.inversePrimary
+                            : Colors.white,
                       ),
                       label: "test"),
                   NavigationDestination(
                       icon: Icon(
                         Icons.groups_3,
                         color: selectedIndex == 2
-                            ? color.onPrimary
-                            : color.inversePrimary,
+                            ? color.inversePrimary
+                            : Colors.white,
                       ),
                       label: "test"),
                   NavigationDestination(
                       icon: Icon(
                         Icons.numbers,
                         color: selectedIndex == 3
-                            ? color.onPrimary
-                            : color.inversePrimary,
+                            ? color.inversePrimary
+                            : Colors.white,
                       ),
                       label: "test")
                 ],
@@ -134,22 +137,38 @@ class _HomeScreenState extends State<HomeScreen> {
                     selectedIndex = value;
                   });
                 },
-                backgroundColor: Colors.white,
-                indicatorColor: Theme.of(context).colorScheme.inversePrimary,
-              ),
+                backgroundColor: Color(0xFF000000),
+                indicatorColor: Colors.transparent,
+              )
             ],
           ),
         ));
   }
 }
 
-class PeopleMatchingPage extends StatelessWidget {
+class PeopleMatchingPage extends StatefulWidget {
   const PeopleMatchingPage({super.key});
+
+  @override
+  State<PeopleMatchingPage> createState() => _PeopleMatchingPageState();
+}
+
+class _PeopleMatchingPageState extends State<PeopleMatchingPage> {
+  OverlayEntry? entry;
+
+  BorderRadiusGeometry radius = const BorderRadius.only(
+    topLeft: Radius.circular(24.0),
+    topRight: Radius.circular(24.0),
+  );
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
+        if (_pc.isPanelOpen) {
+          return;
+        }
+
         activeImage++;
         if (activeImage == imageList.length) {
           activeImage = 0;
@@ -159,18 +178,21 @@ class PeopleMatchingPage extends StatelessWidget {
             duration: Duration(milliseconds: activeImage == 0 ? 600 : 300),
             curve: Curves.easeInOut);
       },
+      onDoubleTap: () {
+        print("double tap");
+      },
+      onVerticalDragUpdate: (details) {
+        var sensitivity = 8;
+        if (details.delta.dy < -sensitivity) {
+          _pc.open();
+        }
+        if (details.delta.dy > sensitivity) {
+          _pc.close();
+        }
+      },
       child: Stack(
         children: [
-          ImageCarousel(),
-          // Container(
-          //   decoration: const BoxDecoration(
-          //       color: Colors.transparent,
-          //       image: DecorationImage(
-          //           image: AssetImage("assets/profileImage1.jpg"),
-          //           fit: BoxFit.fitHeight)),
-          //   height: MediaQuery.of(context).size.height,
-          //   width: MediaQuery.of(context).size.width,
-          // ),
+          const ImageCarousel(),
           Container(
             decoration: BoxDecoration(
                 color: Colors.white,
@@ -178,41 +200,192 @@ class PeopleMatchingPage extends StatelessWidget {
                     begin: FractionalOffset.topCenter,
                     end: FractionalOffset.bottomCenter,
                     colors: [
-                      Colors.black.withOpacity(0.0),
-                      Colors.black.withOpacity(0.0),
+                      Colors.black.withOpacity(0),
+                      Colors.black.withOpacity(0),
                       Colors.black,
                     ],
                     stops: const <double>[
                       0.0,
-                      0.3,
+                      0.2,
                       1.0
                     ])),
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 60),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Irman Zafyree',
-                    style: Theme.of(context).textTheme.displaySmall!.copyWith(
-                        color: Colors.white, fontWeight: FontWeight.bold)),
-                Text("Flutter Developer",
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleLarge!
-                        .copyWith(color: Colors.white)),
-                Text("Bruh, how does this flutter thing work?",
-                    style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                          color: Colors.white,
-                        ))
-              ],
-            ),
+          const ProfileMiniInfo(),
+          SlidingUpPanel(
+            controller: _pc,
+            panel: const ProfileInfo(),
+            minHeight: 0,
+            maxHeight: MediaQuery.of(context).size.height * 0.5,
+            borderRadius: radius,
+            color: Color(0xFF212121),
           )
         ],
       ),
+    );
+  }
+}
+
+class ProfileMiniInfo extends StatelessWidget {
+  const ProfileMiniInfo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Irman Zafyree',
+              style: Theme.of(context)
+                  .textTheme
+                  .displaySmall!
+                  .copyWith(color: Colors.white, fontWeight: FontWeight.bold)),
+          SizedBox(height: 2),
+          Text("Flutter Developer",
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(color: Colors.white)),
+          SizedBox(height: 5),
+          Text("Bruh, how does this flutter thing work?",
+              style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                    color: Colors.white,
+                  ))
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileInfo extends StatelessWidget {
+  const ProfileInfo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var color = Theme.of(context).colorScheme;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("Irman Zafyree",
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineMedium!
+                  .copyWith(color: Colors.white, fontWeight: FontWeight.bold)),
+          Text("Flutter Developer",
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(color: Colors.white)),
+          const SizedBox(height: 20),
+          SizedBox(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Bio",
+                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        color: color.inversePrimary,
+                        fontWeight: FontWeight.bold)),
+                Text("Bruh, how does this flutter thing work?",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyLarge!
+                        .copyWith(color: Colors.white))
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Skills",
+                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        color: color.inversePrimary,
+                        fontWeight: FontWeight.bold)),
+                const Wrap(
+                  spacing: 5,
+                  runSpacing: 5,
+                  children: [
+                    SkillChip(color: Colors.blue, label: "Flutter"),
+                    SkillChip(color: Colors.blue, label: "Flutter"),
+                    SkillChip(color: Colors.blue, label: "Flutter"),
+                    SkillChip(color: Colors.blue, label: "Flutter"),
+                    SkillChip(color: Colors.blue, label: "Flutter"),
+                    SkillChip(color: Colors.blue, label: "Flutter"),
+                    SkillChip(color: Colors.blue, label: "Flutter"),
+                  ],
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Interests",
+                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        color: color.inversePrimary,
+                        fontWeight: FontWeight.bold)),
+                const Wrap(
+                  spacing: 5,
+                  runSpacing: 5,
+                  children: [
+                    SkillChip(color: Colors.blue, label: "Flutter"),
+                    SkillChip(color: Colors.blue, label: "Flutter"),
+                    SkillChip(color: Colors.blue, label: "Flutter"),
+                    SkillChip(color: Colors.blue, label: "Flutter"),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SkillChip extends StatelessWidget {
+  final Color color;
+
+  final String label;
+
+  const SkillChip({
+    super.key,
+    required this.label,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
+      decoration: BoxDecoration(
+          color: color.withOpacity(0.2),
+          borderRadius: BorderRadius.circular(20)),
+      child: Text(label,
+          style: Theme.of(context)
+              .textTheme
+              .bodyLarge!
+              .copyWith(color: Colors.white)),
     );
   }
 }
